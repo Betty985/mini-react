05aee925e3925c7b01bec418f2898b0c5c3d2cb1
Betty985 <1830265753@qq.com>
2022-09-08 16:53:00 +0800

    fibers

--- mreact/render.js
+++ mreact/render.js
@@ -1,28 +1,73 @@
-export function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode('')
-      : document.createElement(element.type);
-const isProperty=key=>key!=='children'
-Object.keys(element.props).filter(isProperty).forEach(name=>{
-    dom[name]=element.props[name]
-})
-  container.appendChild(dom);
-  element.props.children.forEach((child) => {
-    render(child, dom);
-  });
+    fiber.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type);
+  const isProperty = (key) => key !== "children";
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = fiber.props[name];
+    });
+  return dom;
 }
-let nextUnitOfWork=null
-function workLoop(deadline){
-  let shouldYield=false
-  while (nextUnitOfWork&&!shouldYield){
-    nextUnitOfWork=performUnitOfWork(nextUnitOfWork)
-    shouldYield=deadline.timeRemaining()<1
+export function render(element, container) {
+  // nextUnitOfWork设置为fiber的根
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+// 孩子>兄弟>叔叔，到达根节点意味着完成所有渲染工作
+let nextUnitOfWork = null;
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
   }
-  requestIdleCallback(workLoop)
+  requestIdleCallback(workLoop);
 }
 // react使用的是scheduler包
-requestIdleCallback(workLoop)
-function performUnitOfWork(nextUnitOfWork){
-
+requestIdleCallback(workLoop);
+function performUnitOfWork(fiber) {
+  //  add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+  // create new fiber
+  const elements = fiber.props.children;
+  let index = 0,
+    prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if(index===0){
+      fiber.child=newFiber
+    }else{
+      prevSibling.sibling=newFiber
+      index++
+    }
+    if(fiber.child){
+      return fiber.child
+    }
+    let nextFiber=fiber
+    while(nextFiber){
+      if(nextFiber.sibling){
+        return nextFiber.sibling
+      }
+      nextFiber=nextFiber.parent
+    }
+  }
+  // return the unit of work
 }
